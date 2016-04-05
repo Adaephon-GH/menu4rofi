f7d7bbc0633a50320bca1a2c1b6658a096b5c9e6
Adaephon-GH <adaephon@gmx.net>
2016-04-05 21:42:16 +0200

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='menu4rofi',
+    version='0.0.1a1',
+    url='https://github.com/Adaephon-GH/menu4rofi',
+    author='Adaephon',
+    license='MIT',
+    packages=['menu4rofi'],
+#    scripts=['menu4rofi'],
+)
